e920a409475ef5bbc8da74dca4f984f9b5bda861
mixalich004 <mixalich004@gmail.com>
2017-10-14 20:28:54 +0300

    problems run app and create button

---
+++ App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import FBLogin from 'react-native-facebook-login';
+import FBLoginManager from 'react-native-facebook-login';
+
+class Login extends Component {
+  render() {
+    return (
+      <FBLogin />
+    );
+  }
+};
